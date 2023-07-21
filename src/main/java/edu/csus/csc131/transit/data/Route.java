64e5198a5c493d07c8def712ae086abb461b9b09
Chandu Kathi <chandukathi@csus.edu>
2023-07-20 22:40:08 -0700

    [ChanduKathi] Route.java,RouteRepository.java were created and respective code was added

---
+++ src/main/java/edu/csus/csc131/transit/data/Route.java
@@ -0,0 +1,46 @@
+package edu.csus.csc131.transit.data;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Route {
+  @Id
+  private String id;
+  private String shortName;
+  private String longName;
+
+  public Route() {
+    super();
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getShortName() {
+    return shortName;
+  }
+
+  public void getShortName(String shortName) {
+    this.shortName = shortName;
+  }
+
+  public String getLongName() {
+    return longName;
+  }
+
+  public void setLongName(String longName) {
+    this.longName = longName;
+  }
+
+  @Override
+  public String toString() {
+    return "Route [id=" + id + ", shortName=" + shortName + ", longName=" + longName + "]";
+  }
+
+}

a47a73ce9df7b7f36b5124e3877326836af41301
Chandu Kathi <chandukathi@csus.edu>
2023-07-21 20:03:43 -0700

    After creating RouteController and RouteModelAssembler and debugging the route files, the GET request in postman are working

---
+++ src/main/java/edu/csus/csc131/transit/controller/RouteModelAssembler.java
@@ -0,0 +1,20 @@
+package edu.csus.csc131.transit.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import edu.csus.csc131.transit.data.Route;
+
+@Component
+public class RouteModelAssembler implements RepresentationModelAssembler<Route, EntityModel<Route>> {
+
+  @Override
+  public EntityModel<Route> toModel(Route route) {
+    return EntityModel.of(route, //
+        linkTo(methodOn(RouteController.class).getRoute(route.getId())).withSelfRel(),
+        linkTo(methodOn(RouteController.class).getAllRoutes(null, null)).withRel("routes"));
+  }
+}

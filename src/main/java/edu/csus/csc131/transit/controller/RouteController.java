9d8a7e26147241e5e2b8178467cf18ace7f5c366
Chandu Kathi <chandukathi@csus.edu>
2023-07-23 12:35:18 -0700

    [ChanduKathi] Removed unecessary code and debugged and added comments to RouteRepository and RouteController

--- src/main/java/edu/csus/csc131/transit/controller/RouteController.java
+++ src/main/java/edu/csus/csc131/transit/controller/RouteController.java
@@ -11,7 +11,7 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.csus.csc131.transit.controller.common.ResourceNotFoundException;
@@ -31,26 +31,31 @@ public class RouteController {
   }
 
   @GetMapping
-  public CollectionModel<EntityModel<Route>> getAllRoutes(
-      @RequestParam(required = false) String shortName, @RequestParam(required = false) String longName) {
+  // No paramenters need to be passed because we are only searching all or by id
+  public CollectionModel<EntityModel<Route>> getAllRoutes() {
+    // Logs to the terminal
     log.info("Getting all routes.");
 
     List<Route> routes = null;
-    if ((shortName == null || shortName.isBlank()) && (longName == null || longName.isBlank())) {
-      routes = routeRepository.findAll();
-    }
+    /*
+     * Since we are not looking for routes based on any other attribute other
+     * than their id, we can just say findAll
+     */
+    routes = routeRepository.findAll();
 
     log.info("Returning {} routes.", routes.size());
 
+    // After retriving all routes, a collection is sent out via the hyperlink
     // Java Aggregate Operations Tutorial:
     // https://docs.oracle.com/javase/tutorial/collections/streams/index.html
     List<EntityModel<Route>> routeList = routes.stream() //
         .map(routeAssembler::toModel) //
         .toList();
     return CollectionModel.of(routeList,
-        linkTo(methodOn(RouteController.class).getAllRoutes(shortName, longName)).withSelfRel());
+        linkTo(methodOn(RouteController.class).getAllRoutes()).withSelfRel());
   }
 
+  // retrieves and sends the collection of routes
   @GetMapping(value = "/{id}")
   public EntityModel<Route> getRoute(@PathVariable String id) {
     log.info("Getting a route by Id: {}.", id);

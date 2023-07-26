d73013f625ea11300552d756c3890d4c403ab6a0
Chandu Kathi <chandukathi@csus.edu>
2023-07-26 12:26:29 -0700

    [ChanduKathi] fixed typos in Transfer.java

--- src/main/java/edu/csus/csc131/transit/data/Transfer.java
+++ src/main/java/edu/csus/csc131/transit/data/Transfer.java
@@ -4,73 +4,71 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-public class Transfer{
-	@Id
-	private String id;
-	private String fromStopId;
-	private String toStopId;
-	private int minTransferTime;
-	private String fromRouteId;
-	private String toRouteId;
+public class Transfer {
+  @Id
+  private String id;
+  private String fromStopId;
+  private String toStopId;
+  private int minTransferTime;
+  private String fromRouteId;
+  private String toRouteId;
 
-	public Transfer(){
-			super();
-	}
-	public String getId(){
-		return id;
-	}
-	public void setId(string id){
-		this.id = id;
-	}
-	public String getfromStopId()
-	{
-		return fromStopId;
-	}
-	public void setfromStopId(String fromStopId)
-	{
-		this.fromStopId = fromStopId;
-	}
-	public String gettoStopId()
-	{
-		return toStopID;
-	}
-	public void settoStopId(String toStopId)
-	{
-		this.toStopId = toStopid;
-	}
-	public int getminTransferTime()
-	{
-		return minTransferTime;
-	}
-	public void setminTransferTime(int minTransferTime)
-	{
-		this.=minTransferTime = minTransferTime;
-	}
-	public String getfromRouteId()
-	{
-		return fromRouteId;
-	}
-	public void setfromRouteId(String fromRouteId)
-	{
-		this.fromRouteId = fromRouteId;
-	}
-	public String gettoRouteId()
-	{
-		return toRouteId;
-	}
-	public void settoRouteId(String toRouteId)
-	{
-		this.toRouteId = toRouteId;
-	}
+  public Transfer() {
+    super();
+  }
 
-	@Override
-	public string toString()
-	{
-		return "Transfer [id=" + id +", fromStopId=" + fromStopId +", toStopId=" + toStopId +
-				", minTransferTime=" + minTransferTime +", fromRouteId=" +fromRouteId +", toRouteId=" +toRouteId"]";
-	}
+  public String getId() {
+    return id;
+  }
 
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getfromStopId() {
+    return fromStopId;
+  }
+
+  public void setfromStopId(String fromStopId) {
+    this.fromStopId = fromStopId;
+  }
+
+  public String gettoStopId() {
+    return toStopId;
+  }
+
+  public void settoStopId(String toStopId) {
+    this.toStopId = toStopId;
+  }
+
+  public int getminTransferTime() {
+    return minTransferTime;
+  }
+
+  public void setminTransferTime(int minTransferTime) {
+    this.minTransferTime = minTransferTime;
+  }
+
+  public String getfromRouteId() {
+    return fromRouteId;
+  }
+
+  public void setfromRouteId(String fromRouteId) {
+    this.fromRouteId = fromRouteId;
+  }
+
+  public String gettoRouteId() {
+    return toRouteId;
+  }
+
+  public void settoRouteId(String toRouteId) {
+    this.toRouteId = toRouteId;
+  }
+
+  @Override
+  public String toString() {
+    return "Transfer [id=" + id + ", fromStopId=" + fromStopId + ", toStopId=" + toStopId +
+        ", minTransferTime=" + minTransferTime + ", fromRouteId=" + fromRouteId + ", toRouteId=" + toRouteId + "]";
+  }
 
 }
-
-
